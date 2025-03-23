6e6b99f739d626d00ee78673889c8c7c2a8b816a
Filip M. Nowak <github@temp.oneiroi.net>
2025-03-23 21:39:57 +0100

    WIP

---
+++ lib/archive_gh_org_repos/clone.ex
@@ -0,0 +1,30 @@
+defmodule ArchiveGHOrgRepos.Clone do
+  use GenServer
+
+  @impl true
+  def init(root_dir, reflect_path \\ true, add_git_postfix \\ true) do
+    {:ok,
+     %{
+       root_dir: root_dir,
+       reflect_path: reflect_path,
+       add_git_postfix: add_git_postfix
+     }}
+  end
+
+  @impl true
+  def handle_call({:clone_repo, org, repo}, _from, state) do
+    {:reply, clone(org, repo, state.root_dir, state.reflect_path, state.add_git_postfix), state}
+  end
+
+  def clone(org, repo, root_dir, true, true) do
+    System.cmd(
+      "git",
+      [
+        "clone",
+        "https://github.com/#{org}/#{repo}.git",
+        root_dir <> "/" <> "github.com/#{org}/#{repo}.git"
+      ],
+      lines: 8192
+    )
+  end
+end
